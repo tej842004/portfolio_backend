88789e4ec274d378f5a16fef0faffd5c8e59fdf8
Prashant Chauhan <imtej08@gmail.com>
2025-06-15 19:36:17 +0530

    make a meaningful error message

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
   const token = req.header("x-auth-token");
-  if (!token) return res.status(401).send("Access denied. No token provided.");
+  if (!token) return res.status(401).send("Access denied. Please Login.");
 
   try {
     const decode = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
