e15b58593f25013bc036a6c9ba02ea1b0d4c4fae
Prashant <prashant.nthcloud@gmail.com>
2025-06-18 19:12:49 +0530

    implement the like feature

--- models/like.js
+++ models/like.js
@@ -1,29 +1,49 @@
 const mongoose = require("mongoose");
 
-const likeSchema = new mongoose.Schema({
-  user: {
-    type: new mongoose.Schema({
-      _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-      },
-      name: {
-        type: String,
-        minlength: 5,
-        maxlength: 255,
-        required: true,
-      },
-      email: {
-        type: String,
-        minlength: 5,
-        maxlength: 255,
-        required: true,
-      },
-    }),
+const likeSchema = new mongoose.Schema(
+  {
+    user: {
+      type: new mongoose.Schema({
+        _id: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+        },
+        name: {
+          type: String,
+          minlength: 5,
+          maxlength: 255,
+          required: true,
+        },
+        email: {
+          type: String,
+          minlength: 5,
+          maxlength: 255,
+          required: true,
+        },
+      }),
+      required: true,
+    },
+    blog: {
+      type: new mongoose.Schema({
+        _id: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+        },
+        title: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50,
+        },
+      }),
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
+
+likeSchema.index({ user: 1, blog: 1 }, { unique: true });
 
 const Like = mongoose.model("Like", likeSchema);
 
 exports.Like = Like;
-
--- routes/likes.js
+++ routes/likes.js
@@ -1,24 +1,74 @@
-const auth = require("../middleware/auth");
-const { Like } = require("../models/like");
+// const auth = require("../middleware/auth");
+// const { Like } = require("../models/like");
+// const express = require("express");
+// const router = express.Router();
+
+// router.post("/", auth, async (req, res) => {
+//   const like = new Like({
+//     user: {
+//       _id: req.user._id,
+//       name: req.user.name,
+//       email: req.user.email,
+//     },
+//   });
+
+//   await like.save();
+
+//   res.status(201).send(like);
+// });
+
+// router.delete("/", auth, async (req, res) => {
+//   const like = await Like.findOne({ "user._id": req.user._id });
+//   if (!like)
+//     return res.status(400).send("You don't have permission to unlike.");
+
+//   const deleteLike = await Like.findByIdAndDelete(like._id);
+//   res.send(deleteLike);
+// });
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { Like } = require("../models/like");
 
-router.post("/", auth, (req, res) => {
-  const like = new Like({
-    user: {
-      _id: req.user._id,
-      name: req.user.name,
-      email: req.user.email,
-    },
-  });
+// POST /api/likes
+router.post("/", auth, async (req, res) => {
+  const { postId } = req.body;
+  if (!postId) return res.status(400).send("postId is required.");
 
-  like.save();
+  try {
+    const existingLike = await Like.findOne({
+      user: req.user._id,
+      post: postId,
+    });
+    if (existingLike)
+      return res.status(400).send("You already liked this post.");
 
-  res.status(201).send(like);
+    const like = new Like({ user: req.user._id, post: postId });
+    await like.save();
+    res.status(201).send(like);
+  } catch (err) {
+    res.status(500).send("Something went wrong.");
+  }
 });
 
-router.delete("/", auth, (req, res) => {
-  res.send("Delete");
+// DELETE /api/likes
+router.delete("/", auth, async (req, res) => {
+  const { postId } = req.body;
+  if (!postId) return res.status(400).send("postId is required.");
+
+  try {
+    const like = await Like.findOneAndDelete({
+      user: req.user._id,
+      post: postId,
+    });
+    if (!like) return res.status(400).send("Like not found.");
+    res.send(like);
+  } catch (err) {
+    res.status(500).send("Something went wrong.");
+  }
 });
 
 module.exports = router;
+
